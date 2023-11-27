0a5326baf9c23c56727e8811c3d83d6f60531c3e
JordanPettyy <145278647+JordanPettyy@users.noreply.github.com>
2023-11-26 22:53:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const newsletterForm = document.getElementById('newsletterForm');
-    const confirmationMessage = document.getElementById('confirmationMessage');
+const newsletterForm = document.getElementById('newsletterForm');
+const confirmationMessage = document.getElementById('confirmationMessage');
 
-    newsletterForm.addEventListener('submit', function (event) {
-        event.preventDefault();
+newsletterForm.addEventListener('submit', function (event) {
+event.preventDefault();
 
-        // You can add additional validation here before submitting the form
+const emailInput = document.getElementById('email');
+const userEmail = emailInput.value;
 
-        const emailInput = document.getElementById('email');
-        const userEmail = emailInput.value;
-
-        // For simplicity, just display a confirmation message
-        confirmationMessage.textContent = `Thank you for subscribing, ${userEmail}! :)`;
-        newsletterForm.reset(); // Optional: Clear the form after submission
+confirmationMessage.textContent = `Thank you for subscribing, ${userEmail}! :)`;
+newsletterForm.reset();
     });
 });
